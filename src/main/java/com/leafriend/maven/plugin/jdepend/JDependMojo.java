a9e5ba08dcde800c95e2fbbaabb9c9d69633919f
leafriend <leafriend@gmail.com>
2016-07-18 21:30:21 +0900

    Formatting.

--- src/main/java/com/leafriend/maven/plugin/jdepend/JDependMojo.java
+++ src/main/java/com/leafriend/maven/plugin/jdepend/JDependMojo.java
@@ -1,9 +1,9 @@
 package com.leafriend.maven.plugin.jdepend;
 
+import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugins.annotations.Execute;
 import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
-import org.apache.maven.plugin.AbstractMojo;
 
 @Mojo(name = "check", requiresProject = true)
 @Execute(phase = LifecyclePhase.VERIFY)
